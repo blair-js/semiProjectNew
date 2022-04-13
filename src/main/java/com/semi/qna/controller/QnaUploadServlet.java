5aa6f807bd63dcda1f77ea590c8f00676276f416
Younga-Kwon <youngazang58@gmail.com>
2022-04-13 20:25:16 +0900

    중간 commit

---
+++ src/main/java/com/semi/qna/controller/QnaUploadServlet.java
@@ -0,0 +1,57 @@
+package com.semi.qna.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.semi.common.MyFileRenamePolicy;
+
+/**
+ * Servlet implementation class QnaUploadServlet
+ */
+@WebServlet("/uploadQna.do")
+public class QnaUploadServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public QnaUploadServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		if(ServletFileUpload.isMultipartContent(request)) {
+			int maxSize = 10 * 1024 * 1024;
+			
+			String resources = request.getSession().getServletContext().getRealPath("/resources");
+			
+			String savePath = resources + "\\qna_upfiles\\";
+			System.out.println("savePath : " + savePath);
+			
+			MultipartRequest multiRequest = new MultipartRequest(request, savePath, maxSize, "UTF-8", new MyFileRenamePolicy());
+			
+			String
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
